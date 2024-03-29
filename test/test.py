f2ba4dab1a71f24747caf673607477a460862e38
Uri Shaked <uri@urishaked.com>
2024-03-29 20:46:59 +0300

    fix(test): change test case name
    test_adder → test_project
    
    also update copyright string
    
    This is only a cosmetic change - it doesn't impact any functionality

--- test/test.py
+++ test/test.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: © 2023 Uri Shaked <uri@tinytapeout.com>
+# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
 # SPDX-License-Identifier: MIT
 
 import cocotb
@@ -6,7 +6,7 @@ from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
 @cocotb.test()
-async def test_adder(dut):
+async def test_project(dut):
   dut._log.info("Start")
   
   # Our example module doesn't use clock and reset, but we show how to use them here anyway.
